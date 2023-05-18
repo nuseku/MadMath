e3ccd0f993639b0ce0eacf7e85a09b67c70269f5
miktdc <miktadcakmak@gmail.com>
2023-05-18 18:55:23 -0400

    open google in new tab

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import './Home.css'
 const Home = () =>{
     return(
         <div className = "main-container">
-            <a href='https://www.google.com'> google </a>
+            <a href="https://www.google.com" target="_blank">Google</a>
         </div>
     )
 }
