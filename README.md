137f81c4df87ae51792122746ea3458aa5d8db68
miktdc <miktadcakmak@gmail.com>
2023-05-20 16:09:17 -0400

    Home page form, trying to connect to backend (fail)

--- README.md
+++ README.md
@@ -1,8 +1,28 @@
 # MadMath
+
 Website made for Nuray Kutlu's math students.
 
 Created repository on GitHub and used npx extension for a template (npx create-react-app my-app).
 
+Tool for setting up server-side backend connection (npm install express pg pg-promise).
+
+To run client side server = "node server.js".
+
+# SQL Syntax
+
+Create Table:
+CREATE TABLE users (
+id SERIAL PRIMARY KEY,
+username VARCHAR(255) NOT NULL,
+password VARCHAR(255) NOT NULL
+);
+
+Insert:
+INSERT INTO table_name (column1, ...)
+VALUES (value1, value2, value3, ...);
+
+Delete:
+DELETE FROM table_name WHERE condition;
 
 # Getting Started with Create React App
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,70 @@
-import React from 'react'
-import Header from './Header'
-import './Home.css'
+import React, { useState } from "react";
+import Header from "./Header";
+import "./Home.css";
 
-const Home = () =>{
-    return(
-        
-        <div className = "main-container">
-            <div className = "header">
-                <Header />
+const Home = () => {
+  const [fullName, setFullName] = useState("");
+  const [subject, setSubject] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Send a POST request to the server
+    fetch("/submit-form", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ fullName, subject }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.message); // Display the response message
+        // Reset the form inputs
+        setFullName("");
+        setSubject("");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  return (
+    <div className="main-container">
+      <div className="header">
+        <Header />
+      </div>
+      <div className="welcome">
+        <div className="form_2">
+          <form onSubmit={handleSubmit}>
+            <div className="input-container">
+              <label>Full Name</label>
+              <input
+                type="text"
+                name="fullName"
+                value={fullName}
+                onChange={(e) => setFullName(e.target.value)}
+                required
+              />
             </div>
-            <div className = "welcome">
-                <a href="https://www.google.com" target="_blank">Google</a>
-                <h4> Hello! Welcome to Nuray's tutoring service! Log in to get started.</h4>
+            <div className="input-container">
+              <label>Subject</label>
+              <input
+                type="text"
+                name="subject"
+                value={subject}
+                onChange={(e) => setSubject(e.target.value)}
+                required
+              />
             </div>
-            
+            <div className="button-container">
+              <input type="submit" />
+            </div>
+          </form>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
