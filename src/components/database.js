e8c500c1ca742e296a1ee85261ea2899487800b3
miktdc <miktadcakmak@gmail.com>
2023-08-02 21:28:11 -0400

    fix d

--- src/components/database.js
+++ src/components/database.js
@@ -1,3 +1,20 @@
-import React from "react";
+//Will hold the username and password database array that is used in login
+//This file is ignored by .gitignore, which keeps the username and password confidential
 
-export default database
+//Database connection hidden by .gitignore
+
+// const { Pool } = require("pg");
+
+// const pool = new Pool({
+//   host: "localhost",
+//   user: "postgres",
+//   password: "Phone2259731.",
+//   database: "lunamathdb",
+//   port: 5432,
+// });
+
+// module.exports = pool;
+
+const database = [{ username: "miktad", password: "lunamath" }];
+
+export default database;
